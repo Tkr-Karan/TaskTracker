5fb6be90cf1eb643d620eeee39bfd05ea2f8b95e
Tkr-Karan <karankumar072508@gmail.com>
2023-09-03 20:12:21 +0530

    branch change

--- task-tracker/src/components/TaskForm.jsx
+++ task-tracker/src/components/TaskForm.jsx
@@ -1,19 +1,19 @@
 import { useState } from "react";
 
-export default function TaskForm({addTask}) {
+export default function TaskForm({ addTask }) {
   const [task, setTask] = useState("");
   const [dueDate, setDueDate] = useState("");
 
   const handleSumbit = (e) => {
     e.preventDefault();
-    
+
     const newTask = {
-        id : Date.now(),
-        name: task,
-        dueDate: dueDate,
-        completed: false
-    }
-    
+      id: Date.now(),
+      name: task,
+      dueDate: dueDate,
+      completed: false,
+    };
+
     addTask(newTask);
     setTask("");
     setDueDate("");
@@ -21,20 +21,27 @@ export default function TaskForm({addTask}) {
 
   return (
     <>
-      <form onSubmit={handleSumbit}>
+      <form className="m-2 " onSubmit={handleSumbit}>
         <input
+          className="m-2 rounded-sm pl-2"
           type="text"
           placeholder="Task Name"
           value={task}
           onChange={(e) => setTask(e.target.value)}
         />
         <input
+          className=" m-2 rounded-sm pl-2"
           type="text"
           placeholder="Due Date"
           value={dueDate}
           onChange={(e) => setDueDate(e.target.value)}
         />
-        <button type="submit">Add Task</button>
+        <button
+          className="border-solid border-gray-300 bg-slate-500/20 rounded-md w-[80px] "
+          type="submit"
+        >
+          Add Task
+        </button>
       </form>
     </>
   );
